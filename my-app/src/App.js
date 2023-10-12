bf721a2580b044ca6b6cf0e4079433508c562650
rofiq <abdul.hidayat@asyx.com>
2023-10-12 16:26:07 +0700

    3. State

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,29 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import ComponentFunction from './component/ComponentFunction';
 import ComponentFunctionProperties from './component/ComponentFunctionProperties';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ComponentFunction />
-        <ComponentFunctionProperties name="abdul" age="25" />
-        <ComponentFunctionProperties name="hidayat" age="26" />
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import Timer from './component/State';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Timer start="0"/>
+          <Timer start="10"/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ my-app/src/component/State.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            time: props.start
+        }
+    }
+
+    componentDidMount() {
+        this.addInterval = setInterval( () => this.increase(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.addInterval)
+    }
+
+    increase() {
+        this.setState((state, props) => ({
+            time: parseInt(state.time) + 1
+        }))
+    }
+
+    render() {
+        return (
+            <div>{this.state.time} Detik</div>
+        );
+    }
+}
+
+export default Timer;
